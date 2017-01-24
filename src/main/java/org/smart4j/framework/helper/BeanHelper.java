29d97d22b558452224f0970f2ca4164c9daec196
hackaming <2251275876@qq.com>
2017-01-24 17:32:24 +0800

    Add the beanhelp which used to add all beans into a BEAN_MAP. Add IOCHelp class--not finished yet. Add CollectionUtil.java class to adjudge collection/maps if is is empty or not. Note that it is used the apache commons class.

---
+++ src/main/java/org/smart4j/framework/helper/BeanHelper.java
@@ -0,0 +1,34 @@
+package org.smart4j.framework.helper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smart4j.framework.util.ReflectionUtil;
+
+public class BeanHelper {
+	private static final Logger logger = LoggerFactory.getLogger(BeanHelper.class);
+	private static final Map<Class<?>,Object> BEAN_MAP = new HashMap<Class<?>,Object>();
+	
+	static{
+		Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+		for (Class<?>cls : beanClassSet){
+			Object obj = ReflectionUtil.newInstance(cls);
+			BEAN_MAP.put(cls, obj);
+		}
+	}
+	
+	public static Map<Class<?>,Object> getBeanMap(){
+		return BEAN_MAP;
+	}
+	@SuppressWarnings("unchecked")
+	public static <T> T getBean(Class<T> cls){
+		if (!BEAN_MAP.containsKey(cls)){
+			throw new RuntimeException("Can't get beay by class:" + cls);
+		}
+		return (T) BEAN_MAP.get(cls);
+	}
+
+}
---
+++ src/main/java/org/smart4j/framework/util/CollectionUtil.java
@@ -0,0 +1,26 @@
+package org.smart4j.framework.util;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CollectionUtil {
+	private static final Logger logger = LoggerFactory.getLogger(CollectionUtil.class);
+	public static boolean isEmpty(Collection <?> collection){
+		return CollectionUtils.isEmpty(collection);
+	}
+	public static boolean isNotEmpty(Collection<?> collection){
+		return CollectionUtils.isNotEmpty(collection);
+	}
+	public static boolean isEmpty(Map<?,?> map){
+		return MapUtils.isEmpty(map);
+	}
+	public static boolean isNotEmpty(Map<?,?> map){
+		return MapUtils.isNotEmpty(map);
+	}
+
+}
