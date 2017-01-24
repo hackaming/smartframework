d18e232b44a795d6fc77238f1dc47c30a7f0f0f2
hackaming <2251275876@qq.com>
2017-01-24 15:42:19 +0800

    add controller annotation, classhelper clas(which used to add all the annotated classes into a set)

---
+++ src/main/java/org/smart4j/framework/helper/ClassHelper.java
@@ -0,0 +1,62 @@
+package org.smart4j.framework.helper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smart4j.framework.annotation.Action;
+import org.smart4j.framework.annotation.Controller;
+import org.smart4j.framework.annotation.Service;
+import org.smart4j.framework.util.ClassUtil;
+
+public class ClassHelper {
+	private static final Logger logger = LoggerFactory.getLogger(ClassHelper.class);
+	private static final Set<Class<?>> CLASS_SET;
+	
+	static{
+		String basePackage = ConfigHelper.getAppBasePackage();
+		CLASS_SET = ClassUtil.getClassSet(basePackage);
+	}
+	
+	public static Set<Class<?>> getClassSet(){
+		return CLASS_SET;
+	}
+	public static Set<Class<?>> getServiceClassSet(){
+		Set<Class<?>> classSet = new HashSet<Class<?>>();
+		for (Class<?> cls : CLASS_SET){
+			if (cls.isAnnotationPresent(Service.class)){
+				classSet.add(cls);
+			}
+		}
+		return classSet;
+	}
+	public static Set<Class<?>> getControllerClassSet(){
+		Set<Class<?>> classSet = new HashSet<Class<?>>();
+		for (Class<?> cls : classSet){
+			if (cls.isAnnotationPresent(Controller.class)){
+				classSet.add(cls);
+			}
+		}
+		return classSet;
+	}
+	
+	public static Set<Class<?>> getBeanClassSet(){
+		Set <Class<?>> beanClassSet = new HashSet<Class<?>>();
+		beanClassSet.addAll(getServiceClassSet());
+		beanClassSet.addAll(getControllerClassSet());
+		return beanClassSet;
+	}
+	
+	
+	
+/*	public static Set<Class<?>> getControllerClassSet(){
+		Set<Class<?>> classSet = new HashSet<Class<?>>();
+		for (Class<?> cls:classSet){
+			if (cls.isAnnotationPresent(Controller.class)){
+				classSet.add(cls);
+			}
+		}
+		return classSet;
+	}*/
+}
--- src/main/java/org/smart4j/framework/helper/ConfigHelper.java
+++ src/main/java/org/smart4j/framework/helper/ConfigHelper.java
@@ -7,4 +7,26 @@ import org.smart4j.framework.util.PropsUtil;
 
 public final class ConfigHelper {
 	private static final Properties CONFIG_PROPS=PropsUtil.loadProps(ConfigConstant.CONFIG_FIle);
+	
+	public static String getJdbcDriver(){
+		return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_DRIVER);
+	}
+	public static String getUrl(){
+		return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_URL);
+	}
+	public static String getUser(){
+		return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_USER);
+	}
+	public static String getPassword(){
+		return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.JDBC_PASSWORD);
+	}
+	public static String getAppBasePackage(){
+		return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_ASSET_PATH);
+	}
+	public static String getAppJspPath(){
+		return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_JSP_PATH,"/WEB-INF/view/");
+	}
+	public static String getAppAssetPath(){
+		return PropsUtil.getString(CONFIG_PROPS, ConfigConstant.APP_ASSET_PATH,"/asset/");
+	}
 }
