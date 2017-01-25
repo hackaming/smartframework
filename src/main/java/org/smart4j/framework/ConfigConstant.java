59633681a6c05e8e240d65e41d38ed495c322b45
xianming <xianming@fromhome>
2017-01-25 22:55:44 +0800

    Add a annotation class and modified the configconsant in the interface to added the public static final
    shall i add the public static final?by default it is public...

--- src/main/java/org/smart4j/framework/ConfigConstant.java
+++ src/main/java/org/smart4j/framework/ConfigConstant.java
@@ -1,13 +1,13 @@
 package org.smart4j.framework;
 
 public interface ConfigConstant {
-	String CONFIG_FIle="smart.properties";
-	String JDBC_DRIVER="smart.framework.jdbc.driver";
-	String JDBC_URL="smart.framework.jdbc.url";
-	String JDBC_USER="smart.framework.jdbc.user";
-	String JDBC_PASSWORD="smart.framework.jdbc.password";
+	public static final String CONFIG_FIle="smart.properties";
+	public static final String JDBC_DRIVER="smart.framework.jdbc.driver";
+	public static final String JDBC_URL="smart.framework.jdbc.url";
+	public static final String JDBC_USER="smart.framework.jdbc.user";
+	public static final String JDBC_PASSWORD="smart.framework.jdbc.password";
 	
-	String APP_BASE_PACKAGE="smart.framework.app.base_package";
-	String APP_JSP_PATH="smart.framework.app.jsp_path";
-	String APP_ASSET_PATH="smart.framework.app.asset_path";
+	public static final String APP_BASE_PACKAGE="smart.framework.app.base_package";
+	public static final String APP_JSP_PATH="smart.framework.app.jsp_path";
+	public static final String APP_ASSET_PATH="smart.framework.app.asset_path";
 }
