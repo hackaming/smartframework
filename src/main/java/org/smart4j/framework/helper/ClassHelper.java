02e0b0ffe25c2f33c74213238b5eacbea0ba9009
hackaming <2251275876@qq.com>
2017-02-06 15:31:46 +0800

    added the AOP classes.

--- src/main/java/org/smart4j/framework/helper/ClassHelper.java
+++ src/main/java/org/smart4j/framework/helper/ClassHelper.java
@@ -1,5 +1,6 @@
 package org.smart4j.framework.helper;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -48,7 +49,25 @@ public class ClassHelper {
 		return beanClassSet;
 	}
 	
+	public static Set<Class<?>> getClassSetBySuper(Class<?> superClass){
+		Set<Class<?>> classSet = new HashSet<Class<?>>();
+		for (Class<?> cls:CLASS_SET){
+			if (superClass.isAssignableFrom(cls) && superClass.equals(cls)){
+				classSet.add(cls);
+			}
+		}
+		return classSet;
+	}
 	
+	public static Set<Class<?>> getClassSetByAnnotation(Class<? extends Annotation> annotationClass ){
+		Set<Class<?>> classSet = new HashSet<Class<?>>();
+		for (Class<?> cls:CLASS_SET){
+			if (cls.isAnnotationPresent(annotationClass)){
+				classSet.add(cls);
+			}
+		}
+		return classSet;
+	}
 	
 /*	public static Set<Class<?>> getControllerClassSet(){
 		Set<Class<?>> classSet = new HashSet<Class<?>>();
